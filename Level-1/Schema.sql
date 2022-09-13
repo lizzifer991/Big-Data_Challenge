5d8e0b2dbeebbce6c9ea3030efc6975ecd011188
lizzifer991 <elizabeth.loy88@gmail.com>
2022-09-13 02:14:00 -0500

    final

--- Level-1/Schema.sql
+++ Level-1/Schema.sql
@@ -18,8 +18,19 @@ CREATE TABLE customers (
   customer_count INT
 );
 
+-- vine table
+CREATE TABLE vine_table (
+  review_id TEXT PRIMARY KEY,
+  star_rating INTEGER,
+  helpful_votes INTEGER,
+  total_votes INTEGER,
+  vine TEXT
+);
+
 SELECT * from review_id_table;
 
 SELECT * from products;
 
 SELECT * from customers;
+
+SELECT * from vine_table;
